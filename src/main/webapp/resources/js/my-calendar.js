70e6baef141ac42d3f5c982f3c8a6b3fe3592f59
Tran Le Tuan <tranletuanuit@gmail.com>
2015-07-22 14:27:15 +0700

    Office Spring Version 138

--- src/main/webapp/resources/js/my-calendar.js
+++ src/main/webapp/resources/js/my-calendar.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
 
     var URL = $('#calendar').attr('data');
+
     $('#calendar').fullCalendar({
         header : {
         	left : 'prev,next today',
         	center : 'title',
         	right : 'month,basicWeek'
         },
+        lang: 'vi',
         eventLimit: true,
         events : {
         	url : URL,
